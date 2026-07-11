9168cf06f756ec2f24fa8002ff8be879d314adff
brandonifco <brandonifco@gmail.com>
2026-07-11 12:00:42 -0400

    Extract character hit point resolution

--- src/FiveEGoldBox.Core/Characters/CharacterResolver.cs
+++ src/FiveEGoldBox.Core/Characters/CharacterResolver.cs
@@ -224,48 +224,6 @@ public sealed partial class CharacterResolver
     }
  
 
-    private static int? CalculateMaxHitPoints(
-        ClassDefinition? selectedClass,
-        int level,
-        IReadOnlyDictionary<Ability, int> abilityModifiers)
-    {
-        if (selectedClass is null)
-        {
-            return null;
-        }
-
-        int constitutionModifier = abilityModifiers[Ability.Constitution];
-        int firstLevelHitPoints = (int)selectedClass.HitDie + constitutionModifier;
-
-        if (level == 1)
-        {
-            return firstLevelHitPoints;
-        }
-
-        int additionalHitPointsPerLevel =
-            GetFixedHitPointsAfterFirstLevel(selectedClass.HitDie)
-            + constitutionModifier;
-
-        return firstLevelHitPoints
-            + ((level - 1) * additionalHitPointsPerLevel);
-    }
-
-    private static int GetFixedHitPointsAfterFirstLevel(DieType hitDie)
-    {
-        return hitDie switch
-        {
-            DieType.D6 => 4,
-            DieType.D8 => 5,
-            DieType.D10 => 6,
-            DieType.D12 => 7,
-            _ => throw new InvalidOperationException($"Unsupported class hit die '{hitDie}'.")
-        };
-    }
-
-
-
-
-
 
 
 
